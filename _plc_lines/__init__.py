47bb278a836bb6d73a44fdf734764e4b8a9853d3
Fran Hrzenjak <fran.hrzenjak@gmail.com>
2020-02-28 03:26:43 +0100

    added admin action buttons

--- _plc_lines/__init__.py
+++ _plc_lines/__init__.py
@@ -1,5 +1,13 @@
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
+from admin_action_buttons.admin import ActionButtonsMixin
+
 from .celery import app as celery_app
 
 __all__ = ("celery_app",)
+
+from django.contrib import admin
+
+if ActionButtonsMixin not in admin.ModelAdmin.__bases__:
+    admin.ModelAdmin.__bases__ = (
+            (ActionButtonsMixin,) + admin.ModelAdmin.__bases__)
